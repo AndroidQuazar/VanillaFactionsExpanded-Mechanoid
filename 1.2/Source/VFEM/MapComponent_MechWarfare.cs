02c645a551f293810078574d473272ae30054b33
Taranchuk <nfhfyxer@gmail.com>
2020-11-25 00:47:28 +0200

    fixed pathing bug

--- 1.2/Source/VFEM/MapComponent_MechWarfare.cs
+++ 1.2/Source/VFEM/MapComponent_MechWarfare.cs
@@ -15,24 +15,36 @@ namespace VFEMech
 
         }
 
+        public override void FinalizeInit()
+        {
+            base.FinalizeInit();
+            TerrainPatches.factoryFloors[map] = new HashSet<IntVec3>();
+            foreach (var cell in map.AllCells)
+            {
+                if (cell.GetTerrain(map) == VFEMDefOf.VFE_FactoryPath)
+                {
+                    TerrainPatches.factoryFloors[map].Add(cell);
+                }
+            }
+        }
+
         public override void MapComponentTick()
         {
             base.MapComponentTick();
-            //if (TerrainPatches.factoryFloors.ContainsKey(map))
-            //{
-            //    foreach (var cell in TerrainPatches.factoryFloors[map])
-            //    {
-            //        foreach (var t in map.thingGrid.ThingsListAtFast(cell))
-            //        {
-            //            if (t is Pawn pawn && pawn.health.hediffSet.GetFirstHediffOfDef(VFEMDefOf.VFE_FasterMovement) == null)
-            //            {
-            //                var hediff = HediffMaker.MakeHediff(VFEMDefOf.VFE_FasterMovement, pawn);
-            //                pawn.health.AddHediff(hediff);
-            //                pawn.pather.ResetToCurrentPosition();
-            //            }
-            //        }
-            //    }
-            //}
+            if (TerrainPatches.factoryFloors.ContainsKey(map))
+            {
+                foreach (var cell in TerrainPatches.factoryFloors[map])
+                {
+                    foreach (var t in map.thingGrid.ThingsListAtFast(cell))
+                    {
+                        if (t is Pawn pawn && pawn.health.hediffSet.GetFirstHediffOfDef(VFEMDefOf.VFE_FasterMovement) == null)
+                        {
+                            var hediff = HediffMaker.MakeHediff(VFEMDefOf.VFE_FasterMovement, pawn);
+                            pawn.health.AddHediff(hediff);
+                        }
+                    }
+                }
+            }
         }
     }
 }
