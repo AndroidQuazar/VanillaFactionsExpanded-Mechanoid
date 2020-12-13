049ba8a0d28ea22765fc8a12f9bc962202833317
Taranchuk <nfhfyxer@gmail.com>
2020-12-13 17:04:03 +0200

    fixed harmony patch

--- 1.2/Source/VFEM/HarmonyPatches/MechanoidsObeyOrders.cs
+++ 1.2/Source/VFEM/HarmonyPatches/MechanoidsObeyOrders.cs
@@ -38,7 +38,7 @@ namespace VFE.Mechanoids.HarmonyPatches
     [HarmonyPatch(typeof(WanderUtility), "GetColonyWanderRoot")]
     public static class GetColonyWanderRoot_Patch
     {
-        public static void Postfix(ref IntVec3 __result, Pawn pawn, IntVec3 root, float radius, Func<Pawn, IntVec3, IntVec3, bool> validator, Danger maxDanger)
+        public static void Postfix(ref IntVec3 __result, Pawn pawn)
         {
             if (pawn.RaceProps.IsMechanoid && pawn.Faction == Faction.OfPlayer && __result.IsForbidden(pawn) && pawn.playerSettings.AreaRestriction.ActiveCells.Count() > 0)
             {
