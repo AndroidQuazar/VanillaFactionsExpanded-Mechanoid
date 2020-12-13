e5789ad9247159da96be4e6c49886b3774527573
Taranchuk <nfhfyxer@gmail.com>
2020-12-13 06:40:10 +0200

    work machines now should return to their bases once idle

---
+++ Source/VFECore/Mechanoids/AI/JobGivers/JobGiver_ReturnToStationIdle.cs
@@ -0,0 +1,25 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.AI;
+using VFE.Mechanoids;
+
+namespace VFE.Mechanoids.AI.JobGivers
+{
+	public class JobGiver_ReturnToStationIdle : ThinkNode_JobGiver
+	{
+		protected override Job TryGiveJob(Pawn pawn)
+		{
+			var buildingPosition = pawn.TryGetComp<CompMachine>().myBuilding.Position;
+			if (pawn.Position != buildingPosition)
+			{
+				return JobMaker.MakeJob(JobDefOf.Goto, buildingPosition);
+			}
+			else return JobMaker.MakeJob(JobDefOf.Wait, 60);
+		}
+	}
+}
