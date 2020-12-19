7516a5a06e4adb9a1665aca7b9a359afa9333d8f
Simon Latusek <simon.latusek@gmail.com>
2020-12-19 17:02:45 +0100

    added transpiler to check for hoppers to avoid truncation

--- 1.2/Source/VFEM/VFEMDefOf.cs
+++ 1.2/Source/VFEM/VFEMDefOf.cs
@@ -6,24 +6,25 @@ namespace VFEMech
     [DefOf]
     public static class VFEMDefOf
     {
-        public static FactionDef     VFE_Mechanoid;
-        public static HediffDef      VFE_MechanoidUplink;
-        public static ThingDef       VFE_TrooperStorage;
-        public static JobDef         VFEM_Disassemble;
-        public static TerrainDef     VFE_FactoryPath;
-        public static HediffDef      VFE_FasterMovement;
-        public static TerrainDef     VFE_FactoryFloor;
-        public static ThingDef       VFE_ConduitPylon;
-        public static ThingDef       VFE_ComponentMechanoid;
-        public static ThingDef       VFEM_LongMote_DustPuff;
-        public static ThingDef       VFE_LongRangeMissileLauncher;
-        public static JobDef         VFEM_RefuelSilo;
-        public static ThingDef       VFEM_MissileLeaving;
-        public static ThingDef       VFEM_MissileIncoming;
-        public static WorldObjectDef VFEM_TravelingMissile;
+        public static FactionDef       VFE_Mechanoid;
+        public static HediffDef        VFE_MechanoidUplink;
+        public static ThingDef         VFE_TrooperStorage;
+        public static JobDef           VFEM_Disassemble;
+        public static TerrainDef       VFE_FactoryPath;
+        public static HediffDef        VFE_FasterMovement;
+        public static TerrainDef       VFE_FactoryFloor;
+        public static ThingDef         VFE_ConduitPylon;
+        public static ThingDef         VFE_ComponentMechanoid;
+        public static ThingDef         VFEM_LongMote_DustPuff;
+        public static ThingDef         VFE_LongRangeMissileLauncher;
+        public static JobDef           VFEM_RefuelSilo;
+        public static ThingDef         VFEM_MissileLeaving;
+        public static ThingDef         VFEM_MissileIncoming;
+        public static WorldObjectDef   VFEM_TravelingMissile;
         public static ThingSetMakerDef VFEMech_MechanoidStorageContent;
-        public static LetterDef VFEMech_AcceptVisitors;
-        public static ThingDef VFE_MechLandingBeacon;
+        public static LetterDef        VFEMech_AcceptVisitors;
+        public static ThingDef         VFE_MechLandingBeacon;
+        public static ThingDef         VFEM_HeavyHopper;
 
         public static SoundDef VFE_LongRangeMissile_ExplosionFar;
         public static SoundDef VFE_LongRangeMissile_LaunchSiren;
