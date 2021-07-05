10e113e0cd7eb50350d86cdf93a758bd457be9e9
Simon Latusek <simon.latusek@gmail.com>
2021-07-05 17:44:32 +0200

    transpiler fix

--- 1.3/Source/VFEM/HarmonyPatches/MechSpawn_Patch.cs
+++ 1.3/Source/VFEM/HarmonyPatches/MechSpawn_Patch.cs
@@ -13,24 +13,11 @@ namespace VFEM.HarmonyPatches
     using Verse;
     using VFEMech;
 
-    //[HarmonyPatch]
-    //public class MechSpawn_Patch
-    //{
-    //    [HarmonyTargetMethods]
-    //    public static IEnumerable<MethodBase> TargetMethods()
-    //    {
-    //        yield return typeof(SymbolResolver_RandomMechanoidGroup).GetNestedTypes(AccessTools.all)
-    //                                                             .First(t => t.GetMethods(AccessTools.all)
-    //                                                                       .Any(mi => mi.ReturnType == typeof(bool) && mi.GetParameters()[0].ParameterType == typeof(PawnKindDef)))
-    //                                                             .GetMethods(AccessTools.all)
-    //                                                             .First(mi => mi.ReturnType == typeof(bool));
-    //        
-    //        yield return typeof(MechClusterGenerator).GetNestedTypes(AccessTools.all).MaxBy(t => t.GetMethods(AccessTools.all).Length).GetMethods(AccessTools.all)
-    //                                              .First(mi => mi.ReturnType == typeof(bool) && mi.GetParameters()[0].ParameterType == typeof(PawnKindDef));
-    //    }
-    //
-    //    [HarmonyPostfix]
-    //    public static void Postfix(PawnKindDef __0, ref bool __result) => 
-    //        __result &= !__0.race.defName.StartsWith("VFE_Mech");
-    //}
+    [HarmonyPatch(typeof(MechClusterGenerator), nameof(MechClusterGenerator.MechKindSuitableForCluster))]
+    public class MechSpawn_Patch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(PawnKindDef __0, ref bool __result) =>
+            __result &= !__0.race.defName.StartsWith("VFE_Mech");
+    }
 }
--- 1.3/Source/VFEM/HarmonyPatches/Thing_Patch.cs
+++ 1.3/Source/VFEM/HarmonyPatches/Thing_Patch.cs
@@ -12,48 +12,48 @@ namespace VFEMech
     using RimWorld;
     using Verse;
 
-    //[HarmonyPatch(typeof(Thing), nameof(Thing.SpawnSetup))]
-    //public class ThingSpawnSetup_Patch
-    //{
-    //    [HarmonyTranspiler]
-    //    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-    //    {
-    //        List<CodeInstruction> instructionList = instructions.ToList();
-    //        bool                  done            = false;
-    //        FieldInfo             stackCountInfo  = AccessTools.Field(typeof(Thing), nameof(Thing.stackCount));
-    //
-    //
-    //        for (int i = 0; i < instructionList.Count; i++)
-    //        {
-    //            CodeInstruction instruction = instructionList[i];
-    //
-    //            if (!done && instruction.OperandIs(stackCountInfo))
-    //            {
-    //                i += 3;
-    //                yield return new CodeInstruction(OpCodes.Ldarg_1);
-    //                instruction                   = CodeInstruction.Call(typeof(ThingSpawnSetup_Patch), nameof(SpawnSetupHelper));
-    //                instructionList[i + 1].opcode = OpCodes.Brtrue_S;
-    //                done                          = true;
-    //            }
-    //
-    //            yield return instruction;
-    //        }
-    //    }
-    //
-    //    public static bool SpawnSetupHelper(Thing thing, Map map)
-    //    {
-    //        return true;
-    //
-    //
-    //        if (thing.stackCount > thing.def.stackLimit)
-    //        {
-    //            Log.Message(string.Join(" | ", thing.Position.GetThingList(map).Select(t => t.def.label)));
-    //            return false;
-    //        }
-    //
-    //        return true;
-    //    }
-    //}
+    [HarmonyPatch(typeof(Thing), nameof(Thing.SpawnSetup))]
+    public class ThingSpawnSetup_Patch
+    {
+        [HarmonyTranspiler]
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> instructionList = instructions.ToList();
+            bool done = false;
+            FieldInfo stackCountInfo = AccessTools.Field(typeof(Thing), nameof(Thing.stackCount));
+
+
+            for (int i = 0; i < instructionList.Count; i++)
+            {
+                CodeInstruction instruction = instructionList[i];
+
+                if (!done && instruction.OperandIs(stackCountInfo))
+                {
+                    i += 3;
+                    yield return new CodeInstruction(OpCodes.Ldarg_1);
+                    instruction = CodeInstruction.Call(typeof(ThingSpawnSetup_Patch), nameof(SpawnSetupHelper));
+                    instructionList[i + 1].opcode = OpCodes.Brtrue_S;
+                    done = true;
+                }
+
+                yield return instruction;
+            }
+        }
+
+        public static bool SpawnSetupHelper(Thing thing, Map map)
+        {
+            return true;
+
+
+            if (thing.stackCount > thing.def.stackLimit)
+            {
+                Log.Message(string.Join(" | ", thing.Position.GetThingList(map).Select(t => t.def.label)));
+                return false;
+            }
+
+            return true;
+        }
+    }
 
     [HarmonyPatch(typeof(Thing), nameof(Thing.PostMapInit))]
     public class ThingPostInit_Patch
