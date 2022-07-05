6ded846a9ec3dcbb92408d0e912c7522877fb1c0
Taranchuk <nfhfyxer@gmail.com>
2022-07-05 17:24:30 +0300

    changed null check to Any

--- 1.3/Source/VFEM/VFEMWorldComponent.cs
+++ 1.3/Source/VFEM/VFEMWorldComponent.cs
@@ -45,7 +45,7 @@ namespace VFEMech
 
                     IncidentParms parms = StorytellerUtility.DefaultParmsNow(IncidentCategoryDefOf.ThreatBig, this.world);
 
-                    if (cachedIncidents is null)
+                    if (cachedIncidents.Any())
                     {
                         cachedIncidents = MechShipsMod.settings.mechShipIncidentChances
                             .Select(kvp => KeyValuePair.Create(DefDatabase<IncidentDef>.GetNamed(kvp.Key, false), kvp.Value))
